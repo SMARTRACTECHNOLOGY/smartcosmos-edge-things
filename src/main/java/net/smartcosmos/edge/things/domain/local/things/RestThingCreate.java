251e94a7efbb494a242bafcd1ea939e4a7c50005
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-12 16:20:15 +0200

    Add things local service models

---
+++ src/main/java/net/smartcosmos/edge/things/domain/local/things/RestThingCreate.java
@@ -0,0 +1,42 @@
+package net.smartcosmos.edge.things.domain.local.things;
+
+import java.beans.ConstructorProperties;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Data Transfer Object for REST local to create a new Thing.
+ */
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ApiModel(description = "Create a \"Thing\" in the Things Server.")
+public class RestThingCreate
+{
+    private static final int VERSION = 1;
+    @Setter(AccessLevel.NONE)
+    private int version = VERSION;
+
+    @ApiModelProperty(notes = "The programmer provided URN for the Thing. This must be unique to the tenant. If not provided, it will be + " +
+        "generated. Size is database implementation dependent.", required = false)
+    private String urn;
+
+    @ApiModelProperty(notes = "Default: true.")
+    private Boolean active = true;
+
+    @Builder // used in tests
+    @ConstructorProperties({ "urn", "active"})
+    public RestThingCreate(String urn, String type, Boolean active) {
+        this.urn = urn;
+        this.active = active != null ? active : true;
+        this.version = VERSION;
+    }
+}
