0cd9e53d666dc2aadca8f33d7389dea7a0652d6a
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-12 16:14:29 +0200

    Activate formatter registry

--- src/main/java/net/smartcosmos/edge/things/ThingEdgeRdao.java
+++ src/main/java/net/smartcosmos/edge/things/ThingEdgeRdao.java
@@ -1,11 +1,16 @@
 package net.smartcosmos.edge.things;
 
+import java.util.Map;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.format.FormatterRegistrar;
+import org.springframework.format.FormatterRegistry;
 
 import net.smartcosmos.edge.things.config.ThingsEdgeConfiguration;
 import net.smartcosmos.spring.EnableSmartCosmos;
@@ -17,22 +22,21 @@ import net.smartcosmos.spring.EnableSmartCosmosSecurity;
 @EnableSmartCosmosSecurity
 @EnableSwagger2
 @Import(ThingsEdgeConfiguration.class)
-@ComponentScan(basePackages={"net.smartcosmos.edge"}) // required, because @EnableSmartCosmosExtension only scans net.smartcosmos.(dao|extension)
-//@EnabledSmartCosmosEdgeService // could be the annotation that enables the component scan for net.smartcosmos.edge from above
+@ComponentScan(basePackages = { "net.smartcosmos.edge" }) // required, because @EnableSmartCosmosExtension only scans net.smartcosmos.(dao|extension)
+// @EnabledSmartCosmosEdgeService // could be the annotation that enables the component scan for net.smartcosmos.edge from above
 public class ThingEdgeRdao extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ThingEdgeRdao.class).web(true).run(args);
     }
 
-    //    @Autowired
-    //    Map<String, FormatterRegistrar> formatterRegistrarMap;
-    //
-    //    @Override
-    //    public void addFormatters(FormatterRegistry registry) {
-    //        for (FormatterRegistrar registrar : formatterRegistrarMap.values()) {
-    //            registrar.registerFormatters(registry);
-    //        }
-    //    }
+    @Autowired
+    Map<String, FormatterRegistrar> formatterRegistrarMap;
 
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        for (FormatterRegistrar registrar : formatterRegistrarMap.values()) {
+            registrar.registerFormatters(registry);
+        }
+    }
 }
