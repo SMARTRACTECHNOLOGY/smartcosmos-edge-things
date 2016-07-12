9cdb44b44e2679537f353c04667b104cadb64832
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-12 10:04:57 +0200

    Remove @NoArgsConstructor

--- src/main/java/net/smartcosmos/edge/things/domain/RestEdgeThingCreateResponseDto.java
+++ src/main/java/net/smartcosmos/edge/things/domain/RestEdgeThingCreateResponseDto.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import org.apache.commons.lang.BooleanUtils;
 
@@ -18,7 +17,6 @@ import org.apache.commons.lang.BooleanUtils;
  */
 @Data
 @JsonIgnoreProperties({ "version" })
-@NoArgsConstructor
 @ApiModel(description = "Create a \"Thing\" with embedded metadata in the Objects Server.")
 public class RestEdgeThingCreateResponseDto {
     public static final int VERSION_1 = 1;
