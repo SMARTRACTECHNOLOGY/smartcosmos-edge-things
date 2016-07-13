ebd4460fa2edd9779bd0f19dc2e723b200a0ef6d
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-13 08:06:09 +0200

    Delete unused ResultCode class

--- src/main/java/net/smartcosmos/edge/things/ResultCode.java
+++
@@ -1,7 +0,0 @@
-package net.smartcosmos.edge.things;
-
-public class ResultCode {
-    public static final int OK = 1;
-    public static final int ERR_NO_SUCH_URN = -2;
-    public static final int ERR_EMPTY_REQUEST = -16;
-}
