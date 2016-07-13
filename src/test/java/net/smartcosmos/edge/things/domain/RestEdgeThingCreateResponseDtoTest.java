a3bd9912ed44ec45e9a6c4d1640699c2a31af592
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-13 12:26:13 +0200

    Rename and fix response DTO test

---
+++ src/test/java/net/smartcosmos/edge/things/domain/RestEdgeThingCreateResponseDtoTest.java
@@ -0,0 +1,22 @@
+package net.smartcosmos.edge.things.domain;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class RestEdgeThingCreateResponseDtoTest {
+
+    @Test
+    public void thatVersionIsSet() {
+        RestEdgeThingCreateResponseDto dto = RestEdgeThingCreateResponseDto.builder().build();
+
+        assertNotNull(dto.getVersion());
+        assertEquals(RestEdgeThingCreateResponseDto.VERSION_1, dto.getVersion());
+    }
+
+    @Test(expected = NoSuchMethodException.class)
+    public void thatVersionHasNoSetter() throws Exception {
+        RestEdgeThingCreateResponseDto.class.getDeclaredMethod("setVersion", int.class);
+    }
+
+}
