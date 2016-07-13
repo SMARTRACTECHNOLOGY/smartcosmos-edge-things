0528d0dcb31c110fa9c61cf468a7df0093ef7a23
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-13 08:43:49 +0200

    Remove API annotations from local service models, fix lombok annotations

--- src/main/java/net/smartcosmos/edge/things/domain/local/things/RestThingCreate.java
+++ src/main/java/net/smartcosmos/edge/things/domain/local/things/RestThingCreate.java
@@ -3,14 +3,9 @@ package net.smartcosmos.edge.things.domain.local.things;
 import java.beans.ConstructorProperties;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import org.apache.commons.lang.BooleanUtils;
 
@@ -20,25 +15,20 @@ import org.apache.commons.lang.BooleanUtils;
 @Data
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-@ApiModel(description = "Create a \"Thing\" in the Things Server.")
-public class RestThingCreate
-{
+public class RestThingCreate {
+
+    public static final Boolean ACTIVE_DEFAUL_VALUE = true;
+
     private static final int VERSION = 1;
-    @Setter(AccessLevel.NONE)
-    private int version = VERSION;
+    private final int version = VERSION;
 
-    @ApiModelProperty(notes = "The programmer provided URN for the Thing. This must be unique to the tenant. If not provided, it will be + " +
-        "generated. Size is database implementation dependent.", required = false)
     private String urn;
-
-    @ApiModelProperty(notes = "Default: true.")
-    private Boolean active = true;
+    private Boolean active = ACTIVE_DEFAUL_VALUE;
 
     @Builder // used in tests
     @ConstructorProperties({ "urn", "type", "active"})
     public RestThingCreate(String urn, String type, Boolean active) {
         this.urn = urn;
-        this.active = BooleanUtils.toBooleanDefaultIfNull(active, true);
-        this.version = VERSION;
+        this.active = BooleanUtils.toBooleanDefaultIfNull(active, ACTIVE_DEFAUL_VALUE);
     }
 }
--- src/main/java/net/smartcosmos/edge/things/domain/local/things/RestThingUpdate.java
+++ src/main/java/net/smartcosmos/edge/things/domain/local/things/RestThingUpdate.java
@@ -1,39 +1,23 @@
 package net.smartcosmos.edge.things.domain.local.things;
 
-import java.beans.ConstructorProperties;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
- * Data Transfer Object for REST local to update a Thing.
+ * Data Transfer Object for REST request to update a Thing.
  */
 @Data
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-@ApiModel(description = "Update a \"Thing\" in the Objects Server.")
 public class RestThingUpdate
 {
     private static final int VERSION = 1;
-    @Setter(AccessLevel.NONE)
-    private int version = VERSION;
+    private final int version = VERSION;
 
-    @ApiModelProperty(notes = "Default: true")
     private Boolean active;
-
-    @Builder
-    @ConstructorProperties({"active"})
-    public RestThingUpdate(Boolean active)
-    {
-        this.active = active != null ? active : true;
-
-        this.version = VERSION;
-    }
 }
