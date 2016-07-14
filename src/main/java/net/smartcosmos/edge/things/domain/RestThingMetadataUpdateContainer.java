08de7735081d2f7b61df4dbd1f05d9871ba0b249
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-14 16:52:59 +0200

    Add update request body container and converter

---
+++ src/main/java/net/smartcosmos/edge/things/domain/RestThingMetadataUpdateContainer.java
@@ -0,0 +1,15 @@
+package net.smartcosmos.edge.things.domain;
+
+import java.util.Map;
+
+import lombok.Builder;
+import lombok.Data;
+
+import net.smartcosmos.edge.things.domain.local.things.RestThingUpdate;
+
+@Data
+@Builder
+public class RestThingMetadataUpdateContainer {
+    private RestThingUpdate thingRequestBody;
+    private Map<String, Object> metadataRequestBody;
+}
