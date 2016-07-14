00428edb54062fea63a1969af0cfef45640d2c55
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-14 16:41:14 +0200

    Add local service implementations

--- src/main/java/net/smartcosmos/edge/things/service/local/metadata/UpsertMetadataRestServiceDefault.java
+++ src/main/java/net/smartcosmos/edge/things/service/local/metadata/UpsertMetadataRestServiceDefault.java
@@ -1,12 +1,15 @@
 package net.smartcosmos.edge.things.service.local.metadata;
 
 import java.util.Map;
+import javax.inject.Inject;
 
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
+import net.smartcosmos.edge.things.rest.template.metadata.MetadataRestTemplate;
 import net.smartcosmos.security.user.SmartCosmosUser;
 
 /**
@@ -16,9 +19,23 @@ import net.smartcosmos.security.user.SmartCosmosUser;
 @Service
 public class UpsertMetadataRestServiceDefault implements UpsertMetadataRestService {
 
+    private final MetadataRestTemplate restTemplate;
+
+    @Inject
+    public UpsertMetadataRestServiceDefault(MetadataRestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
-    public ResponseEntity<?> upsert(
-        String ownerType, String ownerUrn, Map<String, Object> metadataMap, SmartCosmosUser user) {
-        return null;
+    public ResponseEntity<?> upsert(String ownerType, String ownerUrn, Map<String, Object> metadataMap, SmartCosmosUser user) {
+        try {
+            return restTemplate.upsert(ownerType, ownerUrn, metadataMap);
+        } catch (HttpClientErrorException e) {
+            // if something goes wrong, forward the response
+            return ResponseEntity
+                .status(e.getStatusCode())
+                .headers(e.getResponseHeaders())
+                .body(e.getResponseBodyAsString());
+        }
     }
 }
--- src/main/java/net/smartcosmos/edge/things/service/local/things/UpdateThingRestServiceDefault.java
+++ src/main/java/net/smartcosmos/edge/things/service/local/things/UpdateThingRestServiceDefault.java
@@ -1,11 +1,15 @@
 package net.smartcosmos.edge.things.service.local.things;
 
+import javax.inject.Inject;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import net.smartcosmos.edge.things.domain.local.things.RestThingUpdate;
+import net.smartcosmos.edge.things.rest.template.thing.ThingRestTemplate;
 import net.smartcosmos.security.user.SmartCosmosUser;
 
 /**
@@ -15,9 +19,23 @@ import net.smartcosmos.security.user.SmartCosmosUser;
 @Service
 public class UpdateThingRestServiceDefault implements UpdateThingRestService {
 
+    private final ThingRestTemplate restTemplate;
+
+    @Inject
+    public UpdateThingRestServiceDefault(ThingRestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
-    public ResponseEntity<?> update(
-        String type, String urn, RestThingUpdate thingUpdate, SmartCosmosUser user) {
-        return null;
+    public ResponseEntity<?> update(String type, String urn, RestThingUpdate thingUpdate, SmartCosmosUser user) {
+        try {
+            return restTemplate.update(type, urn, thingUpdate);
+        } catch (HttpClientErrorException e) {
+            // if something goes wrong, forward the response
+            return ResponseEntity
+                .status(e.getStatusCode())
+                .headers(e.getResponseHeaders())
+                .body(e.getResponseBodyAsString());
+        }
     }
 }
