f6a935f8ab8ad33e107f675534cef6b1dc3b0cf8
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-07-15 09:58:31 +0200

    Add GET /type/urn endpoint + corresponding interface method in edge service

--- src/main/java/net/smartcosmos/edge/things/resource/GetThingResource.java
+++ src/main/java/net/smartcosmos/edge/things/resource/GetThingResource.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import net.smartcosmos.security.user.SmartCosmosUser;
 import net.smartcosmos.spring.SmartCosmosRdao;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @SmartCosmosRdao
 @Slf4j
@@ -29,6 +31,15 @@ public class GetThingResource {
         this.getThingService = getThingService;
     }
 
+    @RequestMapping(value = "{type}/{urn}", method = GET, produces = APPLICATION_JSON_UTF8_VALUE, consumes = APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<?> findSpecific(
+        @PathVariable("type") String type,
+        @PathVariable("urn") String urn,
+        SmartCosmosUser user) {
+
+        return getThingService.getByTypeAndUrn(type, urn, user);
+    }
+
     @RequestMapping(value = "", method = RequestMethod.GET, produces = APPLICATION_JSON_UTF8_VALUE, consumes = APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<?> findAll(
         @RequestParam(required = false, defaultValue = "1") Integer page,
@@ -37,6 +48,6 @@ public class GetThingResource {
         @RequestParam(required = false, defaultValue = "created") String sortBy,
         SmartCosmosUser user) {
 
-        return getThingService.getAll(user, page, size, sortOrder, sortBy);
+        return getThingService.getAll(page, size, sortOrder, sortBy, user);
     }
 }
--- src/main/java/net/smartcosmos/edge/things/service/GetThingEdgeService.java
+++ src/main/java/net/smartcosmos/edge/things/service/GetThingEdgeService.java
@@ -4,7 +4,30 @@ import org.springframework.http.ResponseEntity;
 
 import net.smartcosmos.security.user.SmartCosmosUser;
 
+/**
+ * Methods for looking up things and their associated metadata.
+ */
 public interface GetThingEdgeService {
 
-    ResponseEntity<?> getAll(SmartCosmosUser user, Integer page, Integer size, String sortOrder, String sortBy);
+    /**
+     * Look up a specific Thing and its Metadata.
+     *
+     * @param type the type of the thing
+     * @param urn the URN of the thing
+     * @param user the Spring embedded {@link SmartCosmosUser}
+     * @return the response entity
+     */
+    ResponseEntity<?> getByTypeAndUrn(String type, String urn, SmartCosmosUser user);
+
+    /**
+     * Look up all Things and their Metadata in the realm of the authenticated user.
+     *
+     * @param page number of the requested page
+     * @param size requested page size
+     * @param sortOrder the sort order
+     * @param sortBy the field to sort by
+     * @param user the Spring embedded {@link SmartCosmosUser}
+     * @return the response entity
+     */
+    ResponseEntity<?> getAll(Integer page, Integer size, String sortOrder, String sortBy, SmartCosmosUser user);
 }
