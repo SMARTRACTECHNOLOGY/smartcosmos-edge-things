4ca54bc59a20ead297ae7e62083b1d8ad12af6cd
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-08-25 10:58:04 +0200

    Move and refactor thing request creation, add tests

---
+++ src/test/java/net/smartcosmos/edge/things/rest/request/ThingRequestFactoryTest.java
@@ -0,0 +1,137 @@
+package net.smartcosmos.edge.things.rest.request;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.mockito.*;
+import org.mockito.runners.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+
+import net.smartcosmos.edge.things.config.SmartCosmosEdgeThingsProperties;
+import net.smartcosmos.edge.things.domain.local.things.RestThingCreate;
+import net.smartcosmos.edge.things.domain.local.things.RestThingUpdate;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ThingRequestFactoryTest {
+
+    @Spy
+    SmartCosmosEdgeThingsProperties edgeThingsProperties;
+
+    @InjectMocks
+    ThingRequestFactory thingRequestFactory;
+
+    private String serviceName;
+
+    @Before
+    public void setUp() {
+
+        serviceName = edgeThingsProperties.getLocal()
+            .getThings();
+    }
+
+    @Test
+    public void createRequest() throws Exception {
+
+        final String type = "myType";
+        final RestThingCreate restThingCreate = RestThingCreate.builder()
+            .build();
+
+        final String expectedUrl = "http://" + serviceName + "/" + type;
+
+        RequestEntity requestEntity = thingRequestFactory.createRequest(type, restThingCreate);
+        assertNotNull(requestEntity);
+
+        assertEquals(HttpMethod.POST, requestEntity.getMethod());
+        assertTrue(requestEntity.hasBody());
+        assertEquals(restThingCreate, requestEntity.getBody());
+
+        String url = requestEntity.getUrl()
+            .toString();
+
+        assertEquals(expectedUrl, url);
+    }
+
+    @Test
+    public void findByTypeRequest() throws Exception {
+
+        final String type = "myType";
+
+        final String expectedUrl = "http://" + serviceName + "/" + type + "?page=&size=";
+
+        RequestEntity requestEntity = thingRequestFactory.findByTypeRequest(type, null, null);
+        assertNotNull(requestEntity);
+
+        assertEquals(HttpMethod.GET, requestEntity.getMethod());
+        assertFalse(requestEntity.hasBody());
+
+        String url = requestEntity.getUrl()
+            .toString();
+
+        assertEquals(expectedUrl, url);
+    }
+
+    @Test
+    public void findSpecificRequest() throws Exception {
+
+        final String type = "myType";
+        final String urn = "myUrn";
+
+        final String expectedUrl = "http://" + serviceName + "/" + type + "/" + urn;
+
+        RequestEntity requestEntity = thingRequestFactory.findSpecificRequest(type, urn);
+        assertNotNull(requestEntity);
+
+        assertEquals(HttpMethod.GET, requestEntity.getMethod());
+        assertFalse(requestEntity.hasBody());
+
+        String url = requestEntity.getUrl()
+            .toString();
+
+        assertEquals(expectedUrl, url);
+    }
+
+    @Test
+    public void updateRequest() throws Exception {
+
+        final String type = "myType";
+        final String urn = "myUrn";
+        final RestThingUpdate body = RestThingUpdate.builder()
+            .build();
+
+        final String expectedUrl = "http://" + serviceName + "/" + type + "/" + urn;
+
+        RequestEntity requestEntity = thingRequestFactory.updateRequest(type, urn, body);
+        assertNotNull(requestEntity);
+
+        assertEquals(HttpMethod.PUT, requestEntity.getMethod());
+        assertTrue(requestEntity.hasBody());
+        assertEquals(body, requestEntity.getBody());
+
+        String url = requestEntity.getUrl()
+            .toString();
+
+        assertEquals(expectedUrl, url);
+    }
+
+    @Test
+    public void deleteRequest() throws Exception {
+
+        final String type = "myType";
+        final String urn = "myUrn";
+
+        final String expectedUrl = "http://" + serviceName + "/" + type + "/" + urn;
+
+        RequestEntity requestEntity = thingRequestFactory.deleteRequest(type, urn);
+        assertNotNull(requestEntity);
+
+        assertEquals(HttpMethod.DELETE, requestEntity.getMethod());
+        assertFalse(requestEntity.hasBody());
+
+        String url = requestEntity.getUrl()
+            .toString();
+
+        assertEquals(expectedUrl, url);
+    }
+}
