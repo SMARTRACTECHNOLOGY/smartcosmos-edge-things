70c102d14121f69a68d964f679f9ee02a34cecf7
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-11-07 08:42:27 +0100

    Add spring-retry

--- src/main/java/net/smartcosmos/edge/things/ThingEdgeService.java
+++ src/main/java/net/smartcosmos/edge/things/ThingEdgeService.java
@@ -4,6 +4,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Import;
+import org.springframework.retry.annotation.EnableRetry;
 
 import net.smartcosmos.annotation.EnableSmartCosmosEvents;
 import net.smartcosmos.annotation.EnableSmartCosmosExtension;
@@ -15,6 +16,7 @@ import net.smartcosmos.edge.things.config.ThingsEdgeConfiguration;
 @EnableSmartCosmosEvents
 @EnableSmartCosmosMonitoring
 @EnableSmartCosmosSecurity
+@EnableRetry
 @EnableSwagger2
 @Import(ThingsEdgeConfiguration.class)
 public class ThingEdgeService {
